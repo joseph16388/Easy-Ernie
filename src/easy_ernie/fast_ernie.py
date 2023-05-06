2294266b1aecb55dff3cb0ba64a12ade7511d8be
XiaoXinYo <1104361313@qq.com>
2023-05-06 19:37:43 +0800

    parentChatId默认值改为'0'

--- src/easy_ernie/fast_ernie.py
+++ src/easy_ernie/fast_ernie.py
@@ -8,25 +8,25 @@ class FastErnie:
     def __init__(self, BAIDUID: str, BDUSS_BFESS: str):
         self.ernie = Ernie(BAIDUID, BDUSS_BFESS)
         self.sessionId = ''
-        self.parentChatId = 0
+        self.parentChatId = '0'
 
     def askStream(self, question: str) -> Generator:
         if not self.sessionId:
             self.sessionId = self.ernie.newConversation(question)
-        for item in self.ernie.askStream(question, self.sessionId, self.parentChatId):
-            self.parentChatId = item.get('botChatId')
-            yield item
+        for data in self.ernie.askStream(question, self.sessionId, self.parentChatId):
+            self.parentChatId = data['botChatId']
+            yield data
     
     def ask(self, question: str) -> dict:
         result = {}
-        for item in self.askStream(question):
-            result = item
+        for data in self.askStream(question):
+            result = data
         del result['done']
         return result
     
     def close(self) -> bool:
         if self.ernie.deleteConversation(self.sessionId):
             self.sessionId = ''
-            self.parentChatId = 0
+            self.parentChatId = '0'
             return True
         return False
