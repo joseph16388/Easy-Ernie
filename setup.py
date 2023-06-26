ad2633cb21c5d0daaf0fac7797bef2e3cd47a57c
XiaoXinYo <1104361313@qq.com>
2023-06-26 18:06:08 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', 'r', encoding='utf-8') as file:
 
 setup(
     name='easy-ernie',
-    version='0.1.4',
+    version='0.1.5',
     description='简洁的调用文心一言的WebAPI',
     long_description=long_description,
     long_description_content_type="text/markdown",
--- src/easy_ernie/__init__.py
+++ src/easy_ernie/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Author: XiaoXinYo
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from .ernie import Ernie
 from .fast_ernie import FastErnie
